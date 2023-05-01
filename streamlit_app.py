3b1deeea7c4823e1e59bf0fae0530f2f0887ce8b
Shiva Sai Ram Mogulla <83214399+sairam9014@users.noreply.github.com>
2023-05-01 14:10:35 +0530

    Update streamlit_app.py
    Added soft drink header menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boild Free-Range Egg')
+streamlit.header('🥣Breakfast Menu')
+streamlit.text('🥑🍞Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boild Free-Range Egg')
+
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
