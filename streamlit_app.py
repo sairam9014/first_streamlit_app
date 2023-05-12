50916b75fc0f9a8c813fb56c8a288a7c941a4bd7
Shiva Sai Ram Mogulla <83214399+sairam9014@users.noreply.github.com>
2023-05-13 00:36:58 +0530

    Update streamlit_app.py
    removed kiwi as default option

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,11 +31,10 @@ streamlit.dataframe(fruits_to_show)
 # new section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    streamlit.write('The user entered ', fruit_choice)
     # importing ruquests to connect fruityvice API with streamlit
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     #streamlit.text(fruityvice_response.json())
