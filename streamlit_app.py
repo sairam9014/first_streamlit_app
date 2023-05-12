319308ec8b1d05bfdf013db01b5de2685b88ebf3
Shiva Sai Ram Mogulla <83214399+sairam9014@users.noreply.github.com>
2023-05-12 13:48:02 +0530

    Update streamlit_app.py
    commented the raw json line

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 # importing ruquests to connect fruityvice API with streamlit
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+################streamlit.text(fruityvice_response.json())
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
