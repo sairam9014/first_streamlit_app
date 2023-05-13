dad3ad6a87e4b7d48bc8e6365d2f8b5a8adc6841
Shiva Sai Ram Mogulla <83214399+sairam9014@users.noreply.github.com>
2023-05-13 11:04:12 +0530

    Update streamlit_app.py
    commented the streamlit stop function

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ except URLError as e:
   streamlit.error()
 
 # adding streamlit stop function
-streamlit.stop()
+# streamlit.stop()
 streamlit.header("The fruit load list contains:")
 # Snowflake related functions
 def get_fruit_load_list():
