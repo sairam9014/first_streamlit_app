193072448295d82f36beb2fa1d9db364bcca2af0
Shiva Sai Ram Mogulla <83214399+sairam9014@users.noreply.github.com>
2023-05-13 08:59:25 +0530

    Update streamlit_app.py
    organized the code using def function

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 
 streamlit.dataframe(fruits_to_show)
-
+# funtion to repeat a code block
+def get_fruityvice_data(this_fruit_choice):
+  # importing ruquests to connect fruityvice API with streamlit
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  #streamlit.text(fruityvice_response.json())
+  # write your own comment -what does the next line do? 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
 # new section to display fruityvice API response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -35,13 +42,9 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else:
-    # importing ruquests to connect fruityvice API with streamlit
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    #streamlit.text(fruityvice_response.json())
-    # write your own comment -what does the next line do? 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     # write your own comment - what does this do?
-    streamlit.dataframe(fruityvice_normalized)
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
 
